5bca601655c3953838fe23e3ab4c466e905040c8
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-07-04 16:23:18 -0300

    Adding end portal animation and script

---
+++ GhostGame/Assets/Scripts/EndPortal.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndPortal : MonoBehaviour {
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+
+		if (col.gameObject.layer == 8) {
+		
+			//Chamar a variavel que conta o numero de inimigos na cena e incrementa-la em 1
+			//OBS: Essa variavel se encontra dentro do GameController
+			Debug.Log("colidiu Inimigo");
+		}
+
+	}
+}
--- GhostGame/Assets/Scripts/Player/PlayerVisible.cs
+++ GhostGame/Assets/Scripts/Player/PlayerVisible.cs
@@ -7,13 +7,13 @@ public class PlayerVisible : MonoBehaviour {
 	float posX;
 	float posY;
 	public Transform trObject;
-	// Use this for initialization
+
 	void Start () {
 		posX = trObject.transform.position.x;
 		posY = trObject.transform.position.y;
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
 		
 	}
