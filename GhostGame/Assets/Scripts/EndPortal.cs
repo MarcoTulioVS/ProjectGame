b134f9893b98ada3dce98c832020e133e6c40692
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-07-05 16:38:56 -0300

    fixing the bug when colliding with portal

--- GhostGame/Assets/Scripts/EndPortal.cs
+++ GhostGame/Assets/Scripts/EndPortal.cs
@@ -4,10 +4,10 @@ using UnityEngine;
 
 public class EndPortal : MonoBehaviour {
 
-	private GameObject player;
+	public GameObject player;
 
 	void Start () {
-		player = GameObject.FindGameObjectWithTag ("Player");
+		
 	}
 	
 
@@ -22,7 +22,6 @@ public class EndPortal : MonoBehaviour {
 			//Quando o portal colidi com algum inimigo, o player(fantasma) sai de dentro do objeto
 			//Logo em seguida o objeto é destruido
 			Enemies.instance.getOutBodyWhenColliderWithPortal (player);
-			Destroy (col.gameObject,0.5f);
 
 			//Verifica se a variavel countEnemies do GameController é igual ao valor da variavel quantMaxEnemies
 			//Caso seja vai pra próxima fase
