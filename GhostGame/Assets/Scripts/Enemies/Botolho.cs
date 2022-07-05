c902180a0d97880b203dc036fbe160351c41ff59
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-07-05 15:35:18 -0300

    Adding botolho logic to show the portal

---
+++ GhostGame/Assets/Scripts/Enemies/Botolho.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Botolho : MonoBehaviour {
+
+	Animator anim;
+	public GameObject endPotal;
+
+	void Start () {
+		anim = GetComponent<Animator> ();	
+	}
+	
+
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+
+		if (col.gameObject.tag == "power") {
+		
+			anim.SetTrigger ("damage");
+			endPotal.SetActive (true);
+			Destroy (gameObject, 1f);
+		
+		}
+			
+	}
+
+
+}
