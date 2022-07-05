74e0f831bc076859a5f388e29b2178f5eb08db7e
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-07-05 13:29:21 -0300

    Adding the portal power red and green

---
+++ GhostGame/Assets/Scripts/PowerPortal.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerPortal : MonoBehaviour {
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+
+		if (col.gameObject.tag == "red" || col.gameObject.tag == "red1") {
+		
+			if (gameObject.name == "powerGreen") {
+			
+				col.GetComponent<SpriteRenderer> ().color = Color.green;
+			
+			}
+		
+		}
+
+		if (col.gameObject.tag == "green" || col.gameObject.tag == "green1") {
+
+			if (gameObject.name == "powerRed") {
+
+				col.GetComponent<SpriteRenderer> ().color = Color.red;
+
+			}
+
+		}
+
+
+
+	}
+}
