53897a4c4c3096d02c3999514c78c931eb7fa722
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-07-08 15:03:37 -0300

    Adding rat move

---
+++ GhostGame/Assets/Scripts/Enemies/RatScript.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RatScript : Enemies {
+
+	Rigidbody2D rb;
+	Animator anim;
+	public GameObject player;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+
+	void Update () {
+		getOutBody (player);
+	}
+
+	void FixedUpdate(){
+	
+		MainController (gameObject.name, rb, anim);
+	
+	}
+}
