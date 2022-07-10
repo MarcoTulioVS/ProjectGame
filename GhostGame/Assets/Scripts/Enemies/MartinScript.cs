fd657bb97666687bcc26da0e71390ca58fb8549c
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-07-10 17:38:45 -0300

    Adding enemy martin with animation and script

---
+++ GhostGame/Assets/Scripts/Enemies/MartinScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MartinScript : Enemies {
+
+	Rigidbody2D rb;
+	Animator anim;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+
+		MainController (gameObject.name, rb, anim);
+
+	}
+}
