3705b9d026d95aaa01558f3071f403f5f31a5c43
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-07-11 16:46:03 -0300

    Adding script BlockMartinVertical to the blockMartinVertical object
    Adding collision when object blockMartinVertical collides with layer ground. If it so, allows Martin move the block to the left or right.

---
+++ GhostGame/Assets/Scripts/BlockMartinVertical.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlockMartinVertical : MonoBehaviour {
+
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter2D(Collision2D col){
+	
+		if (col.gameObject.layer == 9) {
+		
+			gameObject.GetComponent<Rigidbody2D> ().constraints = RigidbodyConstraints2D.FreezeRotation;
+		
+		}
+	
+	
+	}
+}
