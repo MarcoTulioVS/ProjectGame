ca24636459d31e53790c21c97754776c45ec1283
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-07-12 16:46:12 -0300

    Adding effect plataform up and down
    The plataform go up and go down constantly.

---
+++ GhostGame/Assets/Scripts/FallPlataform.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FallPlataform : MonoBehaviour {
+
+
+	bool colUp;
+	Rigidbody2D rb;
+	public float speed;
+
+	void Start () {
+		
+		rb = GetComponent<Rigidbody2D> ();
+	}
+	
+
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+
+		UpDown ();
+
+	}
+
+	void UpDown(){
+	
+		if (colUp) {
+		
+			rb.velocity = new Vector2 (rb.velocity.x, -speed);
+		
+		} else {
+
+			rb.velocity = new Vector2 (rb.velocity.x, speed);
+		
+		}
+	
+	
+	}
+
+
+	void OnTriggerEnter2D(Collider2D col){
+
+		if (col.gameObject.tag == "colUP") {
+		
+			colUp = true;
+		
+		}
+
+		if (col.gameObject.tag == "colDOWN") {
+		
+			colUp = false;
+		
+		}
+
+
+	}
+
+
+
+}
