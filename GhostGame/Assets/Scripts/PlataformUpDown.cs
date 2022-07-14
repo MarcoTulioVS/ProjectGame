eb03b8e969496cf2920c2b09057b339160b80f0c
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-07-14 13:35:03 -0300

    Adding plataform up and down
    That plataform just up and down and don't fall.

---
+++ GhostGame/Assets/Scripts/PlataformUpDown.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlataformUpDown : MonoBehaviour {
+
+
+	bool colUp;
+	public float speed;
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		UpDown ();
+	}
+
+	void UpDown(){
+
+		if (colUp) {
+
+			transform.position += Vector3.down * speed * Time.deltaTime; 
+		
+		} else {
+		
+			transform.position += Vector3.up * speed * Time.deltaTime; 
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+
+		if (col.gameObject.tag == "colUP") {
+		
+			colUp = true;
+		
+		}
+
+		if (col.gameObject.tag == "colDOWN") {
+
+			colUp = false;
+
+		}
+
+		if (col.gameObject.tag == "Player") {
+		
+
+		
+		}
+
+	}
+}
