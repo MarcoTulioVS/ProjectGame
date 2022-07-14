e974d34d2b73cdc2709880542332559b22e74dbd
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-07-14 17:52:06 -0300

    Adding dog script and movement

---
+++ GhostGame/Assets/Scripts/Other/Dog/DogScript.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DogScript : MonoBehaviour {
+
+	Rigidbody2D rb;
+	public float speed;
+	Animator anim;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	}
+	
+
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+
+		Move ();
+
+	}
+
+	void Move(){
+
+		rb.velocity = new Vector2 (speed, rb.velocity.y);
+		anim.SetBool ("walking", true);
+
+	}
+
+
+}
