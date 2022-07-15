611fc13e3c5c03b0ec4b4ed94b933266a3e34a60
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-07-15 12:21:40 -0300

    Adding cat script and movement

---
+++ GhostGame/Assets/Scripts/Other/Cat/CatScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatScript : MonoBehaviour {
+
+	Rigidbody2D rb;
+	public float speed;
+
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();	
+	}
+	
+
+	void Update () {
+		Move ();
+	}
+
+	void Move(){
+
+		rb.velocity = new Vector2 (speed, rb.velocity.y);
+
+	}
+}
