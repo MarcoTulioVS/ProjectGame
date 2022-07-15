766e5c8eab051f551521a378d22b522bae9ed985
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-07-15 12:34:47 -0300

    Adding player collision with cat
    When the player ghost collides with the cat, the cat is scared and run.
    After that, when the cat is out of the scene, he is destroyed.

--- GhostGame/Assets/Scripts/Other/Cat/CatScript.cs
+++ GhostGame/Assets/Scripts/Other/Cat/CatScript.cs
@@ -6,20 +6,37 @@ public class CatScript : MonoBehaviour {
 
 	Rigidbody2D rb;
 	public float speed;
-
+	Animator anim;
 
 	void Start () {
-		rb = GetComponent<Rigidbody2D> ();	
+		rb = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
 	}
 	
 
 	void Update () {
-		Move ();
+		
+	}
+
+	void FixedUpdate(){
+
+		if (Player.instance.scared && Player.instance.hit.name==gameObject.name) {
+		
+			Move ();
+		
+		}
+		
 	}
 
 	void Move(){
 
 		rb.velocity = new Vector2 (speed, rb.velocity.y);
+		anim.SetBool ("walking", true);
+	}
+
+	void OnBecameInvisible(){
+
+		Destroy (gameObject);
 
 	}
 }
