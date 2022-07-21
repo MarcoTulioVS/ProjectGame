2658e54e270001469e97d9445ad46aa26471cd00
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-07-21 17:31:00 -0300

    Adding Gart movement and Attack
    Adding movement and attack to the boss Gart.
    The boss Gart chases the player wherever he is. Or left or right.

---
+++ GhostGame/Assets/Scripts/Enemies/Boss/Boss.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+
+	public float speed;
+	public int life;
+	public bool isRight;
+	public bool activeMove;
+	public float stopDistance;
+
+
+	void Start () {
+
+
+	}
+	
+
+	void Update () {
+		
+	}
+
+	public void Move(Rigidbody2D rb,Animator anim,Vector2 direction){
+	
+		if (activeMove) {
+			
+			if (isRight) {
+		
+				rb.velocity = new Vector2 (speed, rb.velocity.y);
+				anim.SetInteger ("transition", 1);
+				transform.eulerAngles = new Vector2 (0, 0);
+				direction = Vector2.right;
+
+		
+			} else {
+		
+				rb.velocity = new Vector2 (-speed, rb.velocity.y);
+				anim.SetInteger ("transition", 1);
+				transform.eulerAngles = new Vector2 (0, 180);
+				direction = Vector2.left;
+			}
+		}
+	
+	
+	}
+
+	public void OnCollisionWithPlayer(Transform point,Transform pointBack,Vector2 direction,float maxVision,Rigidbody2D rb,Animator anim){
+
+		RaycastHit2D hit = Physics2D.Raycast (point.position, direction, maxVision);
+
+		if (hit.collider != null) {
+			
+			if (hit.transform.CompareTag ("Player")) {
+
+				activeMove = true;
+
+				float distance = Vector2.Distance (transform.position, hit.transform.position);
+
+				if (distance <= stopDistance) {
+				
+					activeMove = false;
+					rb.velocity = Vector2.zero;
+
+					anim.SetInteger ("transition", 2);
+
+				}
+			
+			}
+		
+		}
+
+		RaycastHit2D behindHit = Physics2D.Raycast (pointBack.position, -direction, maxVision);
+
+		if (behindHit.collider != null) {
+		
+			if (behindHit.transform.CompareTag ("Player")) {
+			
+				isRight = !isRight;
+				activeMove = true;
+			
+			}
+		
+		}
+	}
+		
+}
--- GhostGame/Assets/Scripts/Enemies/Boss/GartScript.cs
+++ GhostGame/Assets/Scripts/Enemies/Boss/GartScript.cs
@@ -2,15 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GartScript : MonoBehaviour {
+public class GartScript : Boss {
 
-	// Use this for initialization
+	Rigidbody2D rb;
+	Animator anim;
+	public Transform point;
+	public Transform pointBack;
+	public float maxVision;
+	Vector2 direction;
+
 	void Start () {
-		
+
+		rb = GetComponent<Rigidbody2D> ();
+		anim = GetComponent<Animator> ();
+	
+		isRight = true;
+
+		if (isRight) {
+
+			transform.eulerAngles = new Vector2 (0, 0);
+			direction = Vector2.right;
+
+		} else {
+
+			transform.eulerAngles = new Vector2 (0, 180);
+			direction = Vector2.left;
+
+		}
 	}
 	
-	// Update is called once per frame
+
 	void Update () {
 		
+
 	}
+
+	void FixedUpdate(){
+
+		Move (rb, anim,direction);
+		OnCollisionWithPlayer (point, pointBack,direction,maxVision,rb,anim);
+	}
+
+	void OnDrawGizmos(){
+	
+		Gizmos.DrawRay (point.position,direction*maxVision);
+		Gizmos.DrawRay (pointBack.position, direction*maxVision);
+	
+	}
+
+
 }
