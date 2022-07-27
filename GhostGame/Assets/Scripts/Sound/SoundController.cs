71d11f520cbf55c1687759de71ecf75c5ae03d42
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-07-27 15:09:22 -0300

    Adding SoundController script
    It used for organize the sounds in the game.
    In this case the sounds can be accessed anywhere.

---
+++ GhostGame/Assets/Scripts/Sound/SoundController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundController : MonoBehaviour {
+
+	public static SoundController instance;
+	public AudioClip sfxTest;
+	AudioSource a;
+
+	void Awake(){
+	
+		instance = this;
+
+	}
+
+	void Start () {
+		a = GetComponent<AudioSource> ();
+	}
+	
+
+	void Update () {
+		
+	}
+
+	public void PlaySound(AudioClip audio){
+
+		a.PlayOneShot (audio);
+	
+	}
+}
