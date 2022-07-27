da4364d3f0676fde7dd3205d6f7975bcd6c942c1
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-07-26 21:21:33 -0300

    Adding behaviour of the ghost in menu
    Removing the animation idle and walk and adding the random behaviour of the ghost in scene menu.
    He just play a random animation from time to time.

---
+++ GhostGame/Assets/Scripts/Player/Menu/PlayerMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMenu : MonoBehaviour {
+
+	Animator anim;
+	public List<Transform> listPoints;
+	int posNumber;
+	bool isRunning;
+
+	void Start () {
+
+		isRunning = true;
+		anim = GetComponent<Animator> ();
+		StartCoroutine ("PlayScare");
+
+	}
+	
+
+	void Update () {
+		
+	}
+
+	IEnumerator PlayScare(){
+
+		while (true) {
+
+			int number = Random.Range (2, 4);
+			anim.SetInteger ("transition", number);
+
+			if (number == 3) {
+
+				yield return new WaitForSeconds (0.6f);
+			
+			} else {
+				
+				yield return new WaitForSeconds (2);
+			}
+			posNumber = Random.Range (0, 6); 
+
+			if (posNumber >= 3) {
+			
+				transform.eulerAngles = new Vector2 (0, 0);
+				int leftSide = Random.Range (0, 3);
+				transform.position = listPoints [leftSide].position;
+
+			} else if(posNumber<3) {
+			
+				transform.eulerAngles = new Vector2 (0, 180);
+				int rightSide = Random.Range (3,6);
+				transform.position = listPoints [rightSide].position;
+			}
+		}
+
+	}
+		
+}
