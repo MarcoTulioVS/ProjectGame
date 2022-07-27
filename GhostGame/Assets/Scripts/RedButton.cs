701c650375674e4d3e8e81bbd20cbbc331af1cd0
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-07-27 19:37:02 -0300

    Adding red button
    When enemy "pedrinha" collides with this button, he stay pressioned.
    If the enemy get off the top, stay normal.

---
+++ GhostGame/Assets/Scripts/RedButton.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedButton : MonoBehaviour {
+
+	Animator anim;
+
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+
+	void Update () {
+		
+	}
+
+	void OnCollisionStay2D(Collision2D col){
+
+		if(col.gameObject.tag=="pedrinha"){
+
+			anim.SetBool ("pressed",true);
+
+		}
+
+	}
+
+	void OnCollisionExit2D(Collision2D notCol){
+	
+		if (notCol.gameObject.tag == "pedrinha") {
+		
+			anim.SetBool ("pressed",false);
+		
+		}
+	}
+}
