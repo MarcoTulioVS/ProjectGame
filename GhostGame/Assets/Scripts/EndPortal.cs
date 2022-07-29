08bbaa6fe4d37f19d3bad32af8aa98c9f924a488
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-07-29 18:09:20 -0300

    Disable collision and physic from enemy
    When the enemy collides with final portal disables their collision and physics.
    
    Just so that the variable countEnemies  of the GameController class, count one by one.

--- GhostGame/Assets/Scripts/EndPortal.cs
+++ GhostGame/Assets/Scripts/EndPortal.cs
@@ -6,6 +6,7 @@ public class EndPortal : MonoBehaviour {
 
 	public GameObject player;
 
+
 	void Start () {
 		
 	}
@@ -19,15 +20,21 @@ public class EndPortal : MonoBehaviour {
 
 		if (col.gameObject.layer == 8) {
 
+			//Disabilita colisão e fisica
+			col.GetComponent<Collider2D> ().enabled = false;
+			col.GetComponent<Rigidbody2D> ().bodyType = RigidbodyType2D.Static;
+
 			//Quando o portal colidi com algum inimigo, o player(fantasma) sai de dentro do objeto
 			//Logo em seguida o objeto é destruido
 			Enemies.instance.getOutBodyWhenColliderWithPortal (player);
 			Destroy (col.gameObject, 2f);
 
+
 			SoundController.instance.PlaySound (SoundController.instance.sfxFinalPortal);
 			//Verifica se a variavel countEnemies do GameController é igual ao valor da variavel quantMaxEnemies
 			//Caso seja vai pra próxima fase
 			StartCoroutine("NextLevel");
+
 		}
 
 	}
