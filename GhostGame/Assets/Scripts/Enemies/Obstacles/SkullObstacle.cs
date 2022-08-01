808d8ca205335507d1ad29aaa4d0fd26fc98fb5f
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-08-01 15:32:49 -0300

    Adding skull script, movement  and damage
    The skull moves to left and right. When the enemy collides with skull, the enemy takes damage.

---
+++ GhostGame/Assets/Scripts/Enemies/Obstacles/SkullObstacle.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkullObstacle : MonoBehaviour {
+
+	Rigidbody2D rb;
+	public float speed;
+
+	void Start () {
+		rb = GetComponent<Rigidbody2D> ();	
+	}
+	
+
+	void Update () {
+		
+	}
+
+	void FixedUpdate(){
+	
+		Move ();
+	
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+	
+		//Ao colidir com parede
+		if (col.gameObject.layer == 16) {
+		
+			speed = -speed;
+		
+		}
+
+		//Ao colidir com inimigo
+		if (col.gameObject.layer == 8) {
+
+			GameController.instance.DecrementLife ();
+		
+		}
+	
+	
+	}
+
+	void Move(){
+
+		rb.velocity = new Vector2 (speed, rb.velocity.y);
+
+	}
+
+
+}
