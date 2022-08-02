f6430d47c140634b2196c854ff1d2cd917741440
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-08-02 15:12:02 -0300

    Moving tutorial menu to main scene

--- GhostGame/Assets/Scripts/GameController.cs
+++ GhostGame/Assets/Scripts/GameController.cs
@@ -28,21 +28,7 @@ public class GameController : MonoBehaviour {
 	public Text textMsg;
 	public Text buttonMsg;
 
-	public Image imageEnemy;
-	public Text nameEnemyText;
-	public Text descriptionEnemy;
 
-	public List<string> listNameEnemies;
-	public List<Sprite> listImagesEnemies;
-	public List<string>listDescriptionEnemies;
-
-	int indexNames;
-	int maxCountNames;
-
-	int maxCountDescription;
-	int minCountDescription;
-	int indexDescription;
-	int indexImage;
 
 	void Awake(){
 
@@ -50,7 +36,7 @@ public class GameController : MonoBehaviour {
 
 	}
 	void Start () {
-		maxCountNames = listNameEnemies.Count;
+		
 
 	}
 	
@@ -60,8 +46,6 @@ public class GameController : MonoBehaviour {
 		imageLifeBar.fillAmount = quantLife / 100;
 
 		GetCameraBackToPlayer ();
-		SetNameEnemy ();
-		SetDescriptionEnemy ();
 
 	}
 
@@ -167,149 +151,6 @@ public class GameController : MonoBehaviour {
 		}
 	}
 
-	void SetNameEnemy(){
-
-		nameEnemyText.text = listNameEnemies [indexNames]; 
-	}
-
-
-	void SetDescriptionEnemy(){
-
-		defineMaxMinCountDescription ();
-
-		if (indexDescription <= minCountDescription) {
-
-			indexDescription = minCountDescription;
-			descriptionEnemy.text = listDescriptionEnemies [indexDescription];
-
-
-		} else if(indexDescription>=minCountDescription) {
-		
-			indexDescription = maxCountDescription;
-			descriptionEnemy.text = listDescriptionEnemies [indexDescription];
-		}
-
-		imageEnemy.sprite = listImagesEnemies [indexImage];
-
-	}
-
-	void defineMaxMinCountDescription(){
-
-		switch (nameEnemyText.text) {
-
-			case "SALIVA":
-
-				maxCountDescription = 1;
-				minCountDescription = 0;
-				indexImage = 0;
-				break;
-			case "COLORADO":
-				maxCountDescription = 3;
-				minCountDescription = 2;
-				indexImage = 1;
-				break;
-			case "GOSMA":
-				maxCountDescription = 5;
-				minCountDescription = 4;
-				indexImage = 2;
-				break;
-			case "MARTIN":
-				maxCountDescription = 7;
-				minCountDescription = 6;
-				indexImage = 3;
-				break;
-			case "MEDUSA":
-				maxCountDescription = 9;
-				minCountDescription = 8;
-				indexImage = 4;
-				break;
-			case "PEDRADA":
-				maxCountDescription = 11;
-				minCountDescription = 10;
-				indexImage = 5;
-				break;
-			case "PEDRINHA":
-				maxCountDescription = 13;
-				minCountDescription = 12;
-				indexImage = 6;
-				break;
-			case "PULINHO":
-				maxCountDescription = 15;
-				minCountDescription = 14;
-				indexImage = 7;
-				break;
-			case "RATO":
-				maxCountDescription = 17;
-				minCountDescription = 16;
-				indexImage = 8;
-				break;
-			case "GATO":
-				maxCountDescription = 19;
-				minCountDescription = 18;
-				indexImage = 9;
-				break;
-			case "CACHORRO":
-				maxCountDescription = 21;
-				minCountDescription = 20;
-				indexImage = 10;
-				break;
-			default:
-				break;
-
-		}
-	}
-
-	public void NextName(){
-		
-		indexNames++;
-
-		if (indexNames > maxCountNames - 1) {
-		
-			indexNames = maxCountNames - 1;
-
-
-		}
-
-	}
-
-	public void PreviousName(){
-		
-		indexNames--;
-
-		if (indexNames < 0) {
-
-			indexNames = 0;
-		
-		
-		}
-
-	}
-
-	public void NextDescription(){
-
-		indexDescription++;
-
-		if (indexDescription > maxCountDescription) {
-
-			indexDescription = maxCountDescription;
-
-		}
-
-
-	}
-
-	public void PreviousDescription(){
-
-		indexDescription--;
-
-		if (indexDescription < minCountDescription) {
-
-			indexDescription = minCountDescription;
-
-		}
-
-
-	}
 
 	public void HidePause(){
 
