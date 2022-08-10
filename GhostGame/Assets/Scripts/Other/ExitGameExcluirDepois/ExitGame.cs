566187acb91db1bc2d90f01577329f422ce7f1ab
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-08-09 21:28:01 -0300

    Adding exit game in "final" scene
    This final scene is not the real final scene.
    For the first time will be built a first version of the project.

---
+++ GhostGame/Assets/Scripts/Other/ExitGameExcluirDepois/ExitGame.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitGame : MonoBehaviour {
+
+	//EXCLUIR ESTE SCRIPT DEPOIS
+	//SERVE APENAS PARA SAIR DO JOGO
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		
+	}
+
+	public void Exit(){
+
+		Application.Quit ();
+
+	}
+}
