f94f4ff022b49f692ab09d7f3bfa145bbb4bd72a
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-08-13 15:06:22 -0300

    Disable arrows
    Disable the arrows when the player is inside the object.

---
+++ GhostGame/Assets/Scripts/Other/ArrowTutorial/ArrowTutorialScene.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrowTutorialScene : MonoBehaviour {
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		StartCoroutine ("DisableArrows");
+	}
+
+	IEnumerator DisableArrows(){
+	
+		if (ObjectsGame.instance.insideObject) {
+			yield return new WaitForSeconds (7);
+			gameObject.SetActive (false);
+		}
+
+	
+	}
+}
