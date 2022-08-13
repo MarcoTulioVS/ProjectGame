88050fad259bfaa42757c55f754242a6f71f6ea8
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-08-13 17:46:55 -0300

    Adding icons and arrow to show how to enter inside enemy
    An arrow green is shown in the scene to show the player that he can get inside enemy.
    When the player is inside the enemy the green arrow is disabled.
    So a red arrow is shown as well as the input buttons icons.

---
+++ GhostGame/Assets/Scripts/DisableArrowGreenRed.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableArrowGreenRed : MonoBehaviour {
+
+	public GameObject arrowGreen;
+	public GameObject arrowRed;
+
+	public GameObject controllerOutMouse;
+	public GameObject controllerOutJoystick;
+
+	void Start () {
+		
+	}
+	
+
+	void Update () {
+		StartCoroutine ("DisableArrowRed");
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+
+		if (col.gameObject.tag == "Player") {
+
+			arrowGreen.SetActive (false);
+			arrowRed.SetActive (true);
+			controllerOutMouse.SetActive (true);
+			controllerOutJoystick.SetActive (true);
+		
+		}
+
+	}
+
+	IEnumerator DisableArrowRed(){
+
+		if (Enemies.instance.insideBody) {
+		
+			yield return new WaitForSeconds (5);
+			arrowRed.SetActive (false);
+			controllerOutMouse.SetActive (false);
+			controllerOutJoystick.SetActive (false);
+
+		
+		}
+
+	}
+}
