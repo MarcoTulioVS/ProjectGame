a0330bf8a28246e2aa4c54f46b78dea47f7086e1
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-08-16 18:30:18 -0300

    Creating spike obstacle
    Was created a spike obstacle in the wall that goes and back horizontally.

---
+++ GhostGame/Assets/Scripts/SpikeScript.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeScript : MonoBehaviour {
+
+	public float speed;
+	bool isRight;
+
+	Rigidbody2D rb;
+
+	void Start () {
+		
+		rb = GetComponent<Rigidbody2D>();
+	}
+	
+
+	void Update () {
+		
+
+	}
+
+	void FixedUpdate(){
+
+		GoAndBack ();
+	}
+
+	void GoAndBack(){
+
+		if (isRight) {
+
+			rb.velocity = new Vector2(speed,rb.velocity.y);
+		
+		} else {
+
+			rb.velocity = new Vector2(-speed,rb.velocity.y);
+
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D col){
+	
+		if (col.gameObject.tag == "areaSpikeLeft") {
+		
+			isRight = true;
+		
+		}
+
+		if (col.gameObject.tag == "areaSpikeRight") {
+
+			isRight = false;
+
+		}
+	
+	}
+
+}
