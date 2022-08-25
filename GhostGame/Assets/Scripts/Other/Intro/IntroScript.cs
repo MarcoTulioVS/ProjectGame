d001dc2e56441ffd49430b858b814ddb297d4d8b
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-08-25 01:04:31 -0300

    Adding nomed monster animation
    Adding nomed monster animation and the IntroScript to control the animation of all images in the scene.

---
+++ GhostGame/Assets/Scripts/Other/Intro/IntroScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IntroScript : MonoBehaviour {
+
+	public List<GameObject> characters;
+	public Animator animNomed;
+	public AudioSource soundPortal;
+
+	public AudioSource nomedLaugh;
+
+	void Start () {
+		StartCoroutine ("IntrorAnimation");
+	}
+	
+
+	void Update () {
+		
+	}
+
+	IEnumerator IntrorAnimation(){
+	
+		for (int i = 1; i < characters.Count - 1; i++) {
+
+			soundPortal.Play ();
+			yield return new WaitForSeconds (3.5f);
+			characters [i].SetActive (false);
+			characters [i + 1].SetActive (true);
+			soundPortal.Stop ();
+
+			if (i == 8) {
+
+				nomedLaugh.Play ();
+				yield return new WaitForSeconds (3);
+				animNomed.SetTrigger ("active");
+				yield return new WaitForSeconds (1.16f);
+				
+			}
+		}	
+	
+	
+	}
+}
