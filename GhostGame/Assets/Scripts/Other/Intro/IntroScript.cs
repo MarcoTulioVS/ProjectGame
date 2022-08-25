9f07702f3490625da3746960173f0f25df1fe2dd
Marco Tulio <43174219+MarcoTulioVS@users.noreply.github.com>
2022-08-25 01:44:17 -0300

    Updating nomed animation and intro script
    Now the when the animation ends goes to next scene and play the sound of the portal.

--- GhostGame/Assets/Scripts/Other/Intro/IntroScript.cs
+++ GhostGame/Assets/Scripts/Other/Intro/IntroScript.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class IntroScript : MonoBehaviour {
 
 	public List<GameObject> characters;
 	public Animator animNomed;
-	public AudioSource soundPortal;
+	public Animator animPortal;
 
+	public AudioSource soundPortal;
 	public AudioSource nomedLaugh;
 
 	void Start () {
@@ -29,16 +31,23 @@ public class IntroScript : MonoBehaviour {
 			characters [i + 1].SetActive (true);
 			soundPortal.Stop ();
 
-			if (i == 8) {
+			if (i == 5) {
 
 				nomedLaugh.Play ();
 				yield return new WaitForSeconds (3);
 				animNomed.SetTrigger ("active");
 				yield return new WaitForSeconds (1.16f);
-				
+				animPortal.SetTrigger ("active");
+				yield return new WaitForSeconds (2.5f);
+				NextScene ();
 			}
 		}	
 	
+	}
+
+	void NextScene(){
+	
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().buildIndex + 1);
 	
 	}
 }
